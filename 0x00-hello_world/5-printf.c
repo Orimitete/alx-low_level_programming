d1dc43b7c322cdd396e2e649c75da3fe191306d4
Orimitete <nwagwukachi@gmail.com>
2022-09-08 14:51:04 -0700

    C Helo, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Short description, single line
+ *  Description: Longer description of the function)?
+ *  Return: Description of the returned value
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,);
+	return (0);
+}
