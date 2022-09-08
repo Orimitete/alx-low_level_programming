b199b71971724d2a65705cee3b470049a10b87f6
Orimitete <nwagwukachi@gmail.com>
2022-09-08 14:46:24 -0700

    C Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *  * main - Short description, single line
- *   * Description: Longer description of the function)?
- *    * Return: Description of the returned value
- *     */
+ * main - Short description, single line
+ * Description: Longer description of the function)?
+ * Return: Description of the returned value
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
