c8b75e4d770608f0e52d61855bc7f0251c853eec
Orimitete <nwagwukachi@gmail.com>
2022-10-06 12:02:17 -0700

    malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  *_calloc -allocated memoria for nmeb elemn de zise bytes
@@ -8,19 +8,19 @@ include "main.h"
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
-	unsigned int i;
+char *p;
+unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+if (nmemb == 0 || size == 0)
+return (NULL);
 
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
+p = malloc(nmemb * size);
+if (p == NULL)
+return (NULL);
 
-	for (i = 0; i < nmemb * size; i++)
-		p[i] = 0;
+for (i = 0; i < nmemb * size; i++)
+p[i] = 0;
 
-	return (p)
+return (p);
 
 }
